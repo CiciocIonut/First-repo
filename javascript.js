8259745da3646168bb67fc070b0a87d703ab9393
CiciocIonut <Cicioc.Io.Ionut@student.utcluj.ro>
2024-11-12 11:16:03 +0200

    added initial js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function change() {
+	document.getElementById("name").innerHTML ="Tehnician DIGI"
+	document.getElementById("date_of_birth").innerHTML ="19 ani"
+	document.getElementById("avatar_img").src="./images/avatar-2.png"
+
+	document.getElementById("materii_table").style.display='none'
+	document.getElementById("materii").style.display='none'
+
+	document.getElementById("hobbies").innerHTML ="Realizari"
+	document.getElementById("hobby_1").innerHTML ="Internship embedded programming"
+	document.getElementById("hobby_2").innerHTML ="Participare concurs CTF"
+	//document.getElementById("hobbies_list").style.display='none'
+	
+	document.getElementById("cuprins").style.display='none'
+}
